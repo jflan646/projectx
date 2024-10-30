cc24622807f3fae9293d532d62c4003f19704d72
James Flannery <jflan646@cccc.edu>
2024-10-30 12:04:02 -0400

    Add files via upload

---
+++ Project2/Project2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Header.h"
+using namespace std;
+
+int main() {
+	int num1 = 1;
+	int num2 = 2;
+	int sum = add(num1 , num2);
+	cout << num1 << " + " << num2 << " is " << sum << endl;
+}
